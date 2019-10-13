5302eddc27c6a1706b152259f04d3aa3c859440d
abhirup92 <43106919+abhirup92@users.noreply.github.com>
2019-10-13 22:30:25 +0530

    added the ansible operation and git clone to ec2

--- terraform_test.tf
+++ terraform_test.tf
@@ -8,8 +8,13 @@ resource "aws_instance" "example" {
 key_name = "abhi"
 user_data = <<-EOF
 #!/bin/bash
-sudo zypper install -y git 
+sudo zypper install -y git
+sudo zypper in -y python
+sudo zypper install -y python2-pip
+sudo pip2 install ansible
 git clone https://abhirup92:79a464f5b271b644083b2e6ee9f579e9b9dd1794@github.com/abhirup92/adp-mkpl-test-ABHIRUP.git
+cd adp-mkpl-test-ABHIRUP/
+ansible-playbook node-npm-install.yml
 EOF
 }
 
